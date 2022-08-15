6de4deeb124df611708856aa8b652c74c9521d45
Ryan Large <ryanhudsonlarge13@gmail.com>
2022-08-14 20:47:00 -0700

    Adding in more GET methods for serving html static files

--- index.js
+++ index.js
@@ -7,4 +7,24 @@ app.get('/', (req, res) => {
    res.sendFile(path.join(__dirname + '/public/HTML/home.html'));
 });
 
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/HTML/home.html'));
+});
+
+app.get('/tees', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/HTML/tees.html'));
+});
+
+app.get('/sweaters', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/HTML/sweaters.html'));
+});
+
+app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/HTML/contact.html'));
+});
+
+app.get('/specials', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/HTML/specials.html'));
+});
+
 app.listen(8080, () => console.log('Your app is running on port 8080'));
