91ece6d00b504876810c6a6a69449ff38aaee40f
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-08-14 20:13:24 -0700

    Applying server get request for homepage.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const express = require('express'), path = require('path');
 const app = express();
 
 app.use(express.static('public'));
-app.use(express.static('/HTML'));
 
 app.get('/', (req, res) => {
-  res.sendFile('/HTML/home.html')
+   res.sendFile(path.join(__dirname + '/public/HTML/home.html'));
 });
 
 app.listen(8080, () => console.log('Your app is running on port 8080'));
