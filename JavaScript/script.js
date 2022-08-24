f153063ed6fdb26adb1a0329a0ceb807fa5ee7b8
Ryan Large <ryanhudsonlarge13@gmail.com>
2022-08-23 21:13:16 -0700

    Changing styles and creating templates for pages.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,6 +2,8 @@
 const lines = document.querySelectorAll('span');
 const navToggle = document.querySelector('.toggle');
 const navigation = document.querySelector('nav');
+const sec1 = document.querySelector('.sec-1');
+const sec2 = document.querySelector(".sec-2");
 const h1 = document.querySelector('.sec-1 h1');
 const h2 = document.querySelector(".sec-1 h2");
 const h3 = document.querySelector(".sec-1 h3");
@@ -16,37 +18,43 @@ const toggleNav = () => {
 
 
 //intro
-class Opacity {
+class Display {
     constructor(elem) {
         this.elem = elem;
     }
-
     on() {
         this.elem.style.opacity = '1';
     }
-
     off() {
         this.elem.style.opacity = '0';
     }
+    displayOn() {
+        this.elem.style.display = 'flex';
+    }
+    displayOff() {
+        this.elem.style.display = 'none';
+    }
 };
 
+//This function is called on an interval. It handles the display properties of all elements via the display class constructor above.
 const check = () => {
     if (count === 1) {
-        headingOff = new Opacity(h1).off();
+        headingOff = new Display(h1).off();
         setTimeout(() => {
-            headingOn = new Opacity(h2).on();      
+            headingOn = new Display(h2).on();      
         }, 1000);
     } 
     if (count === 2) {
-        headingOff = new Opacity(h2).off();
+        headingOff = new Display(h2).off();
         setTimeout(() => {
-            headingOn = new Opacity(h3).on();
+            headingOn = new Display(h3).on();
         }, 1000);
     };
     if (count === 3) {
-        headingOff = new Opacity(h3).off();
+        headingOff = new Display(h3).off();
         setTimeout(() => {
-            // productList = new Opacity(products).on();
+            secOff = new Display(sec1).displayOff();
+            secOn = new Display(sec2).displayOn();
         }, 1000);
     }
 };
