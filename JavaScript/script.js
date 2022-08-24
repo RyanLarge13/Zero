70ae66af3900d1f59b7dc9123810f604eb99c10a
Ryan Large <ryanhudsonlarge13@gmail.com>
2022-08-23 20:13:52 -0700

    Adding files, and updating simple Javascript

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -3,7 +3,10 @@ const lines = document.querySelectorAll('span');
 const navToggle = document.querySelector('.toggle');
 const navigation = document.querySelector('nav');
 const h1 = document.querySelector('.sec-1 h1');
+const h2 = document.querySelector(".sec-1 h2");
+const h3 = document.querySelector(".sec-1 h3");
 const secs = document.querySelectorAll('.sec');
+let count = 0;
 
 //mobile hamburger menu
 const toggleNav = () => {
@@ -26,6 +29,32 @@ class Opacity {
         this.elem.style.opacity = '0';
     }
 };
+
+const check = () => {
+    if (count === 1) {
+        headingOff = new Opacity(h1).off();
+        setTimeout(() => {
+            headingOn = new Opacity(h2).on();      
+        }, 1000);
+    } 
+    if (count === 2) {
+        headingOff = new Opacity(h2).off();
+        setTimeout(() => {
+            headingOn = new Opacity(h3).on();
+        }, 1000);
+    };
+    if (count === 3) {
+        headingOff = new Opacity(h3).off();
+        setTimeout(() => {
+            // productList = new Opacity(products).on();
+        }, 1000);
+    }
+};
+
+setInterval(() => {
+    count++;
+    check();
+}, 4000);
 
 //event listeners
 navToggle.addEventListener('click', toggleNav);
