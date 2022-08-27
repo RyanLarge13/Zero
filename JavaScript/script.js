122f25328cc4fb9ca031a4c7de9e0eb1d80e34a7
Ryan Large <ryanhudsonlarge13@gmail.com>
2022-08-27 09:20:28 -0700

    Adding in content for section 3, Displaying cards and finishing up the script to cycle through content.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,12 +2,22 @@
 const lines = document.querySelectorAll('span');
 const navToggle = document.querySelector('.toggle');
 const navigation = document.querySelector('nav');
+const skip = document.querySelector('.skip');
 const sec1 = document.querySelector('.sec-1');
 const sec2 = document.querySelector(".sec-2");
+const sec3 = document.querySelector('.sec-3');
+const sec4 = document.querySelector(".sec-4");
 const h1 = document.querySelector('.sec-1 h1');
 const h2 = document.querySelector(".sec-1 h2");
 const h3 = document.querySelector(".sec-1 h3");
+const navDots = document.querySelectorAll('.dot');
+const rightBtn = document.querySelector('.fa-circle-chevron-right');
+const leftBtn = document.querySelector('.fa-circle-chevron-left');
+const card = document.querySelectorAll('.card');
+
 let count = 0;
+let navCount = 0;
+let direction;
 
 //mobile hamburger menu
 const toggleNav = () => {
@@ -16,7 +26,7 @@ const toggleNav = () => {
 };
 
 
-//intro
+//JS class allowing easy access and readability to changing elements display and opacity.
 class Display {
     constructor(elem) {
         this.elem = elem;
@@ -36,6 +46,7 @@ class Display {
 };
 
 //This function is called on an interval. It handles the display properties of all elements via the display class constructor above.
+
 const check = () => {
     if (count === 1) {
         headingOff = new Display(h1).off();
@@ -50,21 +61,86 @@ const check = () => {
         }, 1000);
     };
     if (count === 3) {
+        killSkip = new Display(skip).off();
         headingOff = new Display(h3).off();
         setTimeout(() => {
             secOff = new Display(sec1).displayOff();
             secOn = new Display(sec2).displayOn();
             setTimeout(() => {
                 secOn = new Display(sec2).on();
+                killSkip = new Display(skip).displayOff();
+                navDots[0].style.backgroundColor = '#fc354c';
+                navDots.forEach(dot => showIcons = new Display(dot).on());
+                showRight = new Display(rightBtn).on();
+                showLeft = new Display(leftBtn).on();
             }, 250);
         }, 1000);
     }
 };
 
-setInterval(() => {
+let time = setInterval(() => {
     count++;
     check();
 }, 3250);
 
+//The next 3 functions handle the direction of which way the sections need to get displayed in animation and displays them using the consructor class above.
+const killSec1 = () => {
+    clearInterval(time);
+    const secArray = [sec2, sec3, sec4];
+    navDots[0].style.backgroundColor = "#fc354c";
+    navDots.forEach((dot) => (showIcons = new Display(dot).on()));
+    showRight = new Display(rightBtn).on();
+    showLeft = new Display(leftBtn).on();
+    disappear = new Display(sec1).displayOff();
+    killSkip = new Display(skip).displayOff();
+    showNextSec(secArray);
+};
+
+const resetStyle = (secArray) => {
+    navDots.forEach(dot => dot.style.backgroundColor = '#0abfbc');
+    secArray.forEach(section => allSecs = new Display(section).off());
+    setTimeout(() => {
+        secArray.forEach(section => allSecs = new Display(section).displayOff());
+    }, 750);
+};
+
+const showNextSec = () => {
+    const secArray = [sec2, sec3, sec4];
+    resetStyle(secArray);
+    setTimeout(() => {
+        flippedTo = new Display(secArray[navCount]).displayOn();
+        setTimeout(() => {
+            showFlippedTo = new Display(secArray[navCount]).on();
+            navDots[navCount].style.backgroundColor = "#fc354c";
+        }, 50);
+    }, 750);
+};
+
+const turnPage = (direction) => {
+    if (direction === 'right' && navCount !== 2) {
+        navCount++;
+        showNextSec(navCount);
+    }
+    if (direction === 'left' && navCount !== 0) {
+        navCount--;
+        showNextSec(navCount);
+    }
+};
+
 //event listeners
 navToggle.addEventListener('click', toggleNav);
+skip.addEventListener('click', killSec1);
+rightBtn.addEventListener('click', () => {
+    direction = 'right';
+    turnPage(direction);
+});
+leftBtn.addEventListener('click', () => {
+    direction = 'left';
+    turnPage(direction);
+});
+navDots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+        navCount = index;
+        showNextSec();
+    });
+});
