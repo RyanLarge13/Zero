d1b50ac7f49f787b8c9a3897c7e8243090e8a87e
Ryan Large <ryanhudsonlarge13@gmail.com>
2022-08-28 09:56:11 -0700

    Begginning to create the functions to handle live form validation all via JS and following the open/close scripting rules.

--- JavaScript/contact.js
+++ JavaScript/contact.js
@@ -3,7 +3,7 @@ const lines = document.querySelectorAll("span");
 const navToggle = document.querySelector(".toggle");
 const navigation = document.querySelector("nav");
 const form = document.querySelector('form');
-const name = document.querySelector('name');
+const yourName = document.getElementById('name');
 const tel = document.getElementById('tel');
 const email = document.getElementById('email');
 const message = document.getElementById('message');
@@ -14,5 +14,22 @@ const toggleNav = () => {
   navigation.classList.toggle("scale");
 };
 
+//These funtions handle what to do when submitting the form
+const handleForm = (e) => {
+  e.preventDefault();
+  form.reset();
+};
+
+//These functions handle what to do with form validation
+const validate = (e) => {
+  let id = e.target.id;
+  
+};
+
 //event listeners
 navToggle.addEventListener("click", toggleNav);
+form.addEventListener('submit', handleForm);
+yourName.addEventListener('keyup', validate);
+email.addEventListener('keyup', validate);
+tel.addEventListener('keyup', validate);
+message.addEventListener('keyup', validate);
