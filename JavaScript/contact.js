77b7a52435b54a54a63752fa45d4f428ba47c963
Ryan Large <ryanhudsonlarge13@gmail.com>
2022-08-28 01:07:00 -0700

    Adding in more basic styling for contact.HTML

--- JavaScript/contact.js
+++ JavaScript/contact.js
@@ -2,6 +2,11 @@
 const lines = document.querySelectorAll("span");
 const navToggle = document.querySelector(".toggle");
 const navigation = document.querySelector("nav");
+const form = document.querySelector('form');
+const name = document.querySelector('name');
+const tel = document.getElementById('tel');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
 
 //mobile hamburger menu
 const toggleNav = () => {
