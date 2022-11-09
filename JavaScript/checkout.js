b22f7b79e1a8c8ac43231b4eff02e8c61c75b13e
RyanLarge13 <ryanhudsonlarge13@gmail.com>
2022-11-08 20:17:48 -0800

    Completing the functionality for storing not only the products name and total price but also tieing along with that the quantity for each product that is purchased and storing it in local storage.

--- JavaScript/checkout.js
+++ JavaScript/checkout.js
@@ -1,3 +1,15 @@
-const totalPrice = document.querySelector('.total');
+let products = [];
+let keys = Object.keys(localStorage);
 
-TOTALPRICE.INNERHTML = ``
+for (let k = 0; k < localStorage.length; k++) {
+  if (localStorage.getItem(keys[k]) !== "product") continue;
+  products.push(keys[k]);
+}
+
+const spliceProducts = () => {
+    products.forEach((item) => {
+        const quantity = item.split(' ').splice(2).join();
+        const product = item.split(' ').splice(0, 2).join(' ');
+    });
+};
+spliceProducts()
